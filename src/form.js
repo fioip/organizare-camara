6c81941cf1d740199bac3b0be68c647f900513ba
Iuhasz P. Fiona <fio.iuhasz@gmail.com>
2023-04-03 23:38:08 +0300

    moved form code into form.js

--- src/form.js
+++ src/form.js
@@ -2,42 +2,70 @@ function addProduct() {
   const form = document.querySelector("form");
 
   form.innerHTML = `
-     <div>
-         <label>Denumire:</label>
-        <input />
-     </div>
-   
-     <div>
-         <label>Categorie:</label>
-         <input />
-     </div>
-   
-     <div>
-         <label>Alergeni:</label>
-         <input />
-     </div>
-   
-     <div>
-         <label>Unitate de masura:</label>
-         <div class="select">
-            <select name="unitate" id="unitate">
-               <option selected disabled>Unitate</option>
-               <option value="mg">mg</option>
-               <option value="g">g</option>
-               <option value="ml">ml</option>
-               <option value="l">l</option>
-            </select>
-          </div>
-     </div>
+  <div class="modal-body">
+  <div class="form-control">
+      <label>Denumire:</label>
+    <input />
+  </div>
 
-     <div>
-         <label>Bucati:</label>
-         <button>-</button>
-         <input value="0" id="totalClicks" />
-         <button>+</button>
-     </div>
+  <div class="form-control">
+      <label>Categorie:</label>
+      <select id="selectCategory">
+        <option value="" selected disabled hidden>Alege categorie</option>
+        <option>Fainoase</option>
+        <option>Dulciuri</option>
+      </select>
+  </div>
+
+  <div class="form-control">
+      <label>Alergeni:</label>
+      <input />
+  </div>
+
+  <div class="form-control">
+      <label>Unitate de masura:</label>
+      <input />
+  </div>
+
+  <div class="quantity">
+    <label>Cantitate</label>
+    <input value ="0" id="quantity" />
+    <button id="increase-value" onClick="increaseInputValue()" type="button">+</button>
+    <button id="decrase-value" onClick="decreaseInputValue()" type="button">-</button>
+  </div>
+</div>
+<div class="modal-footer">
+  <button>Save</>
+</div>
      `;
   return form;
 }
+
+function increaseInputValue(e) {
+  //e.preventDefault();
+  let increaseValueButton = document.getElementById("increase-value");
+  let input = document.getElementById("quantity");
+  increaseValueButton.addEventListener("click", () => {
+    let inputValue = input.value;
+    const newValue = parseInt(inputValue) + 1;
+    //inputValue = newValue;
+    inputValue = newValue;
+    console.warn("click", inputValue, newValue);
+  });
+}
+
+function decreaseInputValue(e) {
+  let decreaseValueButton = document.getElementById("decrase-value");
+  let input = document.getElementById("quantity");
+  decreaseValueButton.addEventListener("click", () => {
+    let inputValue = input.value;
+    const newValue = parseInt(inputValue) - 1;
+    input.value = newValue;
+    console.warn("click", inputValue, newValue);
+  });
+}
+
+const select = document.getElementById("selectCategory");
+var arr = ["Fainoase", "Conserve", "Dulciuri", "Borcane", "Condimente"];
 
 addProduct();
