0d27e6e43ac2f441ff015e75577dd5c3c1b32c27
Andreea Tataru <andreeatataru13@gmail.com>
2023-04-03 09:10:20 +0300

    added select and counter

--- src/index.js
+++ src/index.js
@@ -25,31 +25,75 @@ function addLiveTextBox() {
   // form.method = "GET";
   // form.action = "#";
   form.innerHTML = `
-  <div>
-      <label>Denumire:</label>
-     <input />
-  </div>
-
-  <div>
-      <label>Categorie:</label>
+  
+  <div class="modal-body">
+    <div class="form-control">
+        <label>Denumire:</label>
       <input />
-  </div>
+    </div>
 
-  <div>
-      <label>Alergeni:</label>
-      <input />
-  </div>
+    <div class="form-control">
+        <label>Categorie:</label>
+        <select id="selectCategory">
+          <option value="" selected disabled hidden>Alege categorie</option>
+          <option>Fainoase</option>
+          <option>Dulciuri</option>
+        </select>
+    </div>
 
-  <div>
-      <label>Unitate de masura:</label>
-      <input />
+    <div class="form-control">
+        <label>Alergeni:</label>
+        <input />
+    </div>
+
+    <div class="form-control">
+        <label>Unitate de masura:</label>
+        <input />
+    </div>
+
+    <div class="quantity">
+      <label>Cantitate</label>
+      <input value ="0" id="quantity" />
+      <button id="increase-value" onClick="increaseInputValue()" type="button">+</button>
+      <button id="decrase-value" onClick="decreaseInputValue()" type="button">-</button>
+    </div>
+  </div>
+  <div class="modal-footer">
+    <button>Save</>
   </div>
   `;
 
   return form;
 }
 
+function increaseInputValue(e) {
+  //e.preventDefault();
+  let increaseValueButton = document.getElementById("increase-value");
+  let input = document.getElementById("quantity");
+  increaseValueButton.addEventListener("click", () => {
+    let inputValue = input.value;
+    const newValue = parseInt(inputValue) + 1;
+    //inputValue = newValue;
+    input.value = newValue;
+    console.warn("click", inputValue, newValue);
+  });
+}
+
+function decreaseInputValue(e) {
+  let decreaseValueButton = document.getElementById("decrase-value");
+  let input = document.getElementById("quantity");
+  decreaseValueButton.addEventListener("click", () => {
+    let inputValue = input.value;
+    const newValue = parseInt(inputValue) - 1;
+    input.value = newValue;
+    console.warn("click", inputValue, newValue);
+  });
+}
+
+const select = document.getElementById("selectCategory");
+var arr = ["Fainoase", "Conserve", "Dulciuri", "Borcane", "Condimente"];
+
 openModal();
 closeModal();
-
 addLiveTextBox();
+selectOptions();
