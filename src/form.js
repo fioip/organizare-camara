d576b6e711ed2ac6efd437c4dbec2e071d3de3f3
Andreea Tataru <andreeatataru13@gmail.com>
2023-04-29 20:06:31 +0300

    quntity field functionality resolved

--- src/form.js
+++ src/form.js
@@ -1,56 +1,79 @@
-function addProduct() {
+export function addProduct() {
   const form = document.querySelector("form");
 
   form.innerHTML = `
   <div class="modal-body">
-  <div class="form-control">
-      <label>Denumire</label>
-    <input required/>
-  </div>
+    <div class="form-control">
+        <label>Denumire</label>
+      <input />
+    </div>
 
-  <div class="form-control">
-      <label>Categorie</label>
-      <select id="selectCategory" required>
-        <option value="" selected disabled hidden>Alege categorie</option>
-        <option>Fainoase</option>
-        <option>Conserve</option>
-        <option>Dulciuri</option>
-        <option>Borcane</option>
-        <option>Condimente</option>
-      </select>
-  </div>
+    <div class="form-control">
+        <label>Categorie</label>
+        <select id="selectCategory">
+          <option value="" selected disabled hidden>Alege categorie</option>
+          <option>Fainoase</option>
+          <option>Conserve</option>
+          <option>Dulciuri</option>
+          <option>Borcane</option>
+          <option>Condimente</option>
+        </select>
+    </div>
 
-  <div class="form-control">
-      <label>Alergeni</label>
-      <input required/>
-  </div>
+    <div class="form-control">
+        <label>Alergeni</label>
+        <input />
+    </div>
 
-  <div class="form-control">
-      <label>Unitate de masura</label>
-      <select id="selectCategory" required>
-        <option value="" selected disabled hidden>Alege unitate</option>
-        <option>mg</option>
-        <option>g</option>
-        <option>ml</option>
-        <option>l</option>
-      </select>
-  </div>
+    <div class="form-control">
+        <label>Unitate de masura</label>
+        <select id="selectCategory">
+          <option value="" selected disabled hidden>Alege unitate</option>
+          <option>mg</option>
+          <option>g</option>
+          <option>ml</option>
+          <option>l</option>
+        </select>
+    </div>
 
-  <div class="form-control">
-      <div class="quantity">
-          <label required>Cantitate</label>
-          <button onclick="totalClick(-1)">-</button>
-          <input id="totalClicks" value="0" />
-        <button onclick="totalClick(1)">+</button>
-      </div>
+    <div class="form-control">
+        <div class="quantity">
+            <label>Cantitate</label>
+            <button type="button" id="decrase-value">-</button>
+            <input id="quantity" value="0" />
+          <button type="button" id="increase-value">+</button>
+        </div>
+    </div>
   </div>
+  <div class="modal-footer">
+    <button>Save</>
+  </div>
+`;
 
-</div>
-<div class="modal-footer">
-  <button>Save</>
-</div>
-     `;
+  addEventOnQuantityButtons();
   return form;
 }
 
-addProduct();
+export function addEventOnQuantityButtons() {
+  console.warn("click");
+  let increaseValueButton = document.getElementById("increase-value");
+  increaseValueButton.addEventListener("click", increaseInputValue);
+  let decreaseValueButton = document.getElementById("decrase-value");
+  decreaseValueButton.addEventListener("click", decreaseInputValue);
+}
+
+function increaseInputValue() {
+  let input = document.getElementById("quantity");
+  let inputValue = input.value;
+  const newValue = parseInt(inputValue) + 1;
+  input.value = newValue;
+  console.warn("click", inputValue, newValue);
+}
+
+function decreaseInputValue() {
+  let input = document.getElementById("quantity");
+  let inputValue = input.value;
+  const newValue = parseInt(inputValue) - 1;
+  input.value = newValue;
+  console.warn("click", inputValue, newValue);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { addProduct } from "./form";
+
 function openModal() {
   const triggers = document.querySelectorAll("[data-modal]");
   triggers.forEach((trigger) => {
@@ -18,48 +20,19 @@ function closeModal() {
   });
 }
 
-// function incrementInputValue() {
-//   let increaseValueButton = document.getElementById("plus");
-//   let input = document.getElementById("quantity");
+// function totalClick(click) {
+//   let totalClicks = document.getElementById("totalClicks");
+//   const sum = parseInt(totalClicks.innerText) + click;
+//   totalClicks.innerText = sum;
 
-//   increaseValueButton.addEventListener("click", (e) => {
-//     if (e.target.matches("#plus")) {
-//       let newValue = parseInt(input.value) + 1;
-//       console.warn("New Value: ", newValue, typeof newValue);
-//       console.warn("Input Value b: ", input.value, typeof input.value);
-//       input.value = newValue;
-//       console.warn("Input Value a: ", input.value, typeof input.value);
-//     }
-//   });
+//   if (sum < 0) {
+//     totalClicks.innerText = 0;
+//   }
 // }
-
-// function decrementInputValue() {
-//   let decreaseValueButton = document.getElementById("minus");
-//   let input = document.getElementById("quantity");
-//   decreaseValueButton.addEventListener("click", (e) => {
-//     if (e.target.matches("#minus")) {
-//       console.warn("decrease");
-//     }
-
-//     let inputValue = input.value;
-//     const newValue = parseInt(inputValue) - 1;
-//     input.value = newValue;
-//     console.warn("click", inputValue, newValue);
-//   });
-// }
-
-function totalClick(click) {
-  let totalClicks = document.getElementById("totalClicks");
-  const sum = parseInt(totalClicks.innerText) + click;
-  totalClicks.innerText = sum;
-
-  if (sum < 0) {
-    totalClicks.innerText = 0;
-  }
-}
 
 const select = document.getElementById("selectCategory");
 var arr = ["Fainoase", "Conserve", "Dulciuri", "Borcane", "Condimente"];
 
 openModal();
 closeModal();
+addProduct();
